55a886e04046a37a31e7fd996b001295551aca38
hinoguma <noguchi.kohei.0904@gmail.com>
2020-11-07 00:08:20 +0900

    add talk wrapper and icon component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
 
-  <div class="vlp-flex">
-    <div>aaa</div>
-    <div>bbb</div>
+  <div class="vlp-talk-preview">
+    <div class="vlp-talk-header">
+      <div>プレビュー</div>
+    </div>
+    <div class="vlp-talk-content">
+
+      <div class="vpl-talk-record">
+        <UserIcon />
+      </div>
+
+    </div>
   </div>
 
 </template>
 
 <script>
+import UserIcon from './components/UserIcon'
+
 export default {
   name: 'App',
-  components: {
+  components: { UserIcon },
+  props: {
 
   }
 }
---
+++ src/components/UserIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="vlp-user-icon-wrapper">
+    <img class="vlp-user-icon-img" :src="iconSrc"/>
+  </div>
+</template>
+
+<script>
+import NoProfileImage from '../assets/images/no-image-profile.png'
+
+export default {
+  name: 'UserIcon',
+  props: {
+    iconUrl: { type: String, default: '' }
+  },
+  computed: {
+    iconSrc: function () {
+      if (this.iconUrl !== '') {
+        return this.iconUrl
+      }
+      return NoProfileImage
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
