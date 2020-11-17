1abfda6ea15caa74b132b1c8d4918eb703a8d4c5
hinoguma <noguchi.kohei.0904@gmail.com>
2020-11-17 21:54:59 +0900

    add video message

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,15 @@
 
       </div>
 
+      <div class="vlp-talk-record vlp-flex vlp-flex-wrap">
+        <UserIcon />
+
+        <div class="vlp-talk-message vlp-border-box">
+          <VideoMessage :line-json="{previewImageUrl: 'https://sm.mashable.com/mashable_in/seo/default/8-years-of-the-avengers-how-joss-whedons-risk-became-crucial_5z9w.jpg'}" />
+        </div>
+
+      </div>
+
     </div>
   </div>
 
@@ -33,10 +42,11 @@
 import UserIcon from './components/UserIcon'
 import TextMessage from './components/messages/TextMessage'
 import ImageMessage from './components/messages/ImageMessage'
+import VideoMessage from './components/messages/VideoMessage'
 
 export default {
   name: 'App',
-  components: { ImageMessage, TextMessage, UserIcon },
+  components: { VideoMessage, ImageMessage, TextMessage, UserIcon },
   props: {
 
   }
