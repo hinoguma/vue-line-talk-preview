ec2cdf91c71fa13c8553bcf9fec1a2fbf41c9a87
hinoguma <noguchi.kohei.0904@gmail.com>
2020-11-27 09:43:11 +0900

    add test for TextMessage Component

--- src/components/messages/__tests__/textMesage.test.js
+++ src/components/messages/__tests__/textMesage.test.js
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils'
 import TextMessage from '@/components/messages/TextMessage'
 
-describe('ExampleComponent', () => {
-  it('is a Vue instance', () => {
+describe('TextMessage Component', () => {
+  it('has text props', () => {
     const wrapper = mount(TextMessage, {
       props: {
         lineJson: {
@@ -12,6 +12,18 @@ describe('ExampleComponent', () => {
       }
     })
     expect(wrapper.find('div').exists()).toBe(true)
-    // expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.find('div').text()).toBe('text')
+  })
+
+  it('does not have text props', () => {
+    const wrapper = mount(TextMessage, {
+      props: {
+        lineJson: {
+          type: 'text'
+        }
+      }
+    })
+    expect(wrapper.find('div').exists()).toBe(true)
+    expect(wrapper.find('div').text()).toBe('')
   })
 })
